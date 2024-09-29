8f415a16df133f9051df0657d281ca01ddf4fec1
Keya Loding <loding.keya@gmail.com>
2024-09-29 16:21:33 -0500

    updated processing service to convert videos to 720p

--- video-processing/src/index.ts
+++ video-processing/src/index.ts
@@ -1,11 +1,36 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  // This gets the path of the input video file from the body of the request
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath) {
+    res.status(400).send("Bad request: Missing input file path");
+    return;
+  } else if (!outputFilePath) {
+    res.status(400).send("Bad request: Missing output file path");
+    return;
+  }
+
+  // Convert video to 360p
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:720")
+    .on("end", () => {
+      return res.status(200).send("Video successfully processed.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err}`);
+    })
+    .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+  console.log(`Video processing started at http://localhost:${port}`);
 });
